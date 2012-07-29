ec75e3aedced21babd44f77e7eefc0dd47d58029
Ikram <ikramuddinmiu@gmail.com>
2012-07-29 15:22:11 +0530

    Built the first working Prototype

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,16 @@
 package controllers;
 
-import play.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import play.api.templates.Html;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +18,42 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+    return ok(index.render());
+  }
+  
+  public static Result show(String uname){
+	  String html="";
+	  int i=0;
+	  
+	  try {
+		URL url=new URL("https://api.github.com/users/"+uname+"/repos");
+		HttpURLConnection con=(HttpURLConnection) url.openConnection();
+		InputStream is=con.getInputStream();
+		StringBuilder s=new StringBuilder("");
+		
+		while((i=is.read())!=-1){
+			s.append((char)i);
+		}
+		JSONArray ar=new JSONArray(s.toString());
+		JSONObject o;
+
+		html="<ul style=\"position:absolute; left:40%;\">";
+		for(i=0; i<ar.length();i++){
+			o=ar.getJSONObject(i);
+			html=html.concat("<li>"+o.getString("name")+"</li>");
+		}
+		html=html.concat("</ul>");
+		System.out.println(html);
+		
+	  } catch (MalformedURLException e) {
+			e.printStackTrace();
+	  } catch (IOException e) {
+		e.printStackTrace();
+	  } catch (JSONException e) {
+		e.printStackTrace();
+	  }
+	  
+	  return ok(views.html.show.render(new Html(html),uname));
   }
   
 }
